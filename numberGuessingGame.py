a1a9bddd63d180edd120ee56d8d892ee1938e1cd
Moses Oghene <45708379+officialoghene@users.noreply.github.com>
2020-04-18 23:21:23 +0100

    ValueError Update
    Number guessing game updated with exceptions to catch ValueErrors and render a good message while preventing program from breaking on wrong integer input

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -4,19 +4,22 @@ def difficutlty(level, range):
     guesses = level
     while guesses > 0:
         computer = randint(1, range)
-        user = input("Enter your guess \n>")
-        if user.isalpha():
-            print(f"Sorry, '{user}' is not a valid number")
-        else:
-            if int(user) == computer:
-                points += 3
-                print(f"You got it right! points = {points}")
+        try:
+            user = input("Enter your guess \n>")
+            if user.isalpha():
+                print(f"Sorry, Invalid number '{user}'!")
             else:
-                guesses -= 1
-                print(f"That was wrong! Remaining Guesses = {guesses}")
-                if guesses == 0:
-                    print(f"Game over! Total Points = {points}")
-                    points = 0
+                if int(user) == computer:
+                    points += 3
+                    print(f"You got it right! points = {points}")
+                else:
+                    guesses -= 1
+                    print(f"That was wrong! Remaining Guesses = {guesses}")
+                    if guesses == 0:
+                        print(f"Game over! Total Points = {points}")
+                        points = 0
+        except ValueError:
+            print(f"Invalid number '{user.upper()}'! Only numbers are valid guess")
 
 while True:
     stage = input("\nEnter difficulty level ( 'easy, medium or hard' ) to end game type 'quit'\n>")
